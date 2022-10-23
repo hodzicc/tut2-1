d358a071e0584780074adad6e50e153059bf1247
hodzicc <ahodzic1@etf.unsa.ba>
2022-10-23 18:26:23 +0200

    add git

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -4,10 +4,27 @@ package ba.unsa.etf.rpr;
  * Hello world!
  *
  */
+import java.util.Scanner;
+import java.lang.Math;
 public class App 
 {
+    public static int faktorijel(int n) {
+        int fakt=1;
+        for(int i=n; i>1; i--)
+        {
+            fakt*=i;
+        }
+        return fakt;
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner ulaz =new Scanner(System.in);
+        System.out.println( "Unesite n: " );
+        int n= ulaz.nextInt();
+        System.out.println("Faktorijel broja je: "+faktorijel(n));
+        System.out.println("Sinus broja je: "+Math.sin(n));
+
+
     }
 }
