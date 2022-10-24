689acd19a8a9e4fc5f69afda4487e8bfbea4f2fd
hodzicc <ahodzic1@etf.unsa.ba>
2022-10-24 07:24:16 +0200

    add

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -16,14 +16,24 @@ public class App
         }
         return fakt;
     }
-
+    public static double sinus(int n){
+        double rad=n*1./180.*Math.PI;
+        double sin=rad;
+        for(int i=1; i<=10; i++){
+            if(i%2==0)
+            sin+=Math.pow(rad,2*i+1)/faktorijel(2*i+1);
+            else
+                sin-=Math.pow(rad,2*i+1)/faktorijel(2*i+1);
+        }
+        return sin;
+    }
     public static void main( String[] args )
     {
         Scanner ulaz =new Scanner(System.in);
         System.out.println( "Unesite n: " );
         int n= ulaz.nextInt();
         System.out.println("Faktorijel broja je: "+faktorijel(n));
-        System.out.println("Sinus broja je: "+Math.sin(n));
+        System.out.println("Sinus broja je: "+sinus(n));
 
 
     }
